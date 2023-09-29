5f26b074070340cb6d140d8ec4b2fe0c565d4005
szt2vetmat <szt2vetmat@vasvari.org>
2023-09-29 11:57:43 +0200

    Kész

---
+++ SquareProject/Client/ViewModels/BaseClass/IViewModelBase.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel;
+
+namespace ViewModels.BaseClass
+{
+    public interface IViewModelBase : INotifyPropertyChanged
+    {
+        public bool IsBusy { get; set; }
+    }
+}
